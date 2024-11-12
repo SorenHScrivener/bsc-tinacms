2a09755ba99e25d9770721c78181f21805a20707
Soren H Scrivener <SorenHScrivener@gmail.com>
2024-11-11 23:07:04 -0500

    fixed contact us errot

--- tina/config.js
+++ tina/config.js
@@ -33,7 +33,7 @@ const branch =
   "main";
 
 export default defineConfig({
-  branch: "main",
+  branch: process.env.VERCEL_GIT_COMMIT_REF || '',
 
   // Get this from tina.io
   clientId: process.env.TINA_CLIENT_ID || '',
