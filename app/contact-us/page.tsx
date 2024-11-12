26bf2653877613cb35c1e3c0fe408f4e438ea64e
Soren H Scrivener <SorenHScrivener@gmail.com>
2024-11-11 21:49:49 -0500

    fixed contact us errot

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,8 +1,8 @@
 import PageComponent from "@/components/main";
 import { PageAndNavAndDataQuery } from '@/tina/__generated__/types'
 import { client } from "@/tina/__generated__/client"
-import { tinaField } from 'tinacms/dist/react'
-import { useTina } from 'tinacms/dist/react';
+// import { tinaField } from 'tinacms/dist/react'
+// import { useTina } from 'tinacms/dist/react';
 
 export default async function page(props: PageAndNavAndDataQuery['data']) {
     const result = await client.queries.pageAndNavAndData({ relativePath: "contact-us.md" });
