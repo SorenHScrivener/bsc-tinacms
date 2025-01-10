e8a0595ffbce0852d6fb3e510c62eabff5c143a6
Soren H Scrivener <SorenHScrivener@gmail.com>
2025-01-09 20:48:54 -0500

    blog added

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,16 +1,17 @@
+import React, { Suspense } from "react";
 import PageComponent from "@/components/main";
 // import { PageAndNavAndDataQuery } from '@/tina/__generated__/types'
-import { client } from "@/tina/__generated__/client"
+import { client } from "@/tina/__generated__/client";
 // import { tinaField } from 'tinacms/dist/react'
 // import { useTina } from 'tinacms/dist/react';
 
-export default async function page(
-    // props: PageAndNavAndDataQuery['data']
-) {
+const Page = async () => {
     const result = await client.queries.pageAndNavAndData({ relativePath: "contact-us.md" });
     return (
-        <>
-            <PageComponent  {...result} />
-        </>
-    )
-}
+        <Suspense fallback={<div>Loading...</div>}>
+            <PageComponent {...result} />
+        </Suspense>
+    );
+};
+
+export default Page;
