2c16945998c245ad0c0ef564ecbb424bfbbad87e
Soren H Scrivener <SorenHScrivener@gmail.com>
2025-01-09 20:38:36 -0500

    blog added

--- app/our-strategies/page.tsx
+++ app/our-strategies/page.tsx
@@ -1,11 +1,20 @@
+import React, { Suspense } from 'react';
 import PageComponent from "@/components/main";
-import { client } from "@/tina/__generated__/client"
+import { client } from "@/tina/__generated__/client";
 
-export default async function page() {
+const fetchData = async () => {
     const result = await client.queries.pageAndNavAndData({ relativePath: "our-strategies.md" });
+    return result;
+};
+
+const Page = async () => {
+    const result = await fetchData();
+
     return (
-        <>
+        <Suspense fallback={<div>Loading...</div>}>
             <PageComponent {...result} />
-        </>
-    )
-}
+        </Suspense>
+    );
+};
+
+export default Page;
