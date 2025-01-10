1093cc703e818f03deb44d9c5fe23312a22c021b
Soren H Scrivener <SorenHScrivener@gmail.com>
2025-01-09 21:14:46 -0500

    blog added

--- app/blog-posts/page.tsx
+++ app/blog-posts/page.tsx
@@ -1,10 +1,10 @@
+import React, { Suspense } from "react";
 import PageComponent from "@/components/main";
-import { client } from "@/tina/__generated__/client"
-// import BlogWrapper from "@/components/wrappers/blogWrapper"; 
+import { client } from "@/tina/__generated__/client";
 
-export default async function page(): Promise<JSX.Element> {
+const PageContent = async () => {
     const result = await client.queries.pageAndNavAndData({ relativePath: "blog-posts.md" });
-    const blogPostsResult = await client.queries.blogPostsConnection()
+    const blogPostsResult = await client.queries.blogPostsConnection();
     const blogPosts = blogPostsResult.data.blogPostsConnection.edges?.map((post) => {
         if (post?.node) {
             return {
@@ -21,10 +21,16 @@ export default async function page(): Promise<JSX.Element> {
         const result = await client.queries.blogPosts({ relativePath: `${post!.slug}.mdx` });
         return result;
     }) || []);
-    
+
     const data = result.data as { blogPosts?: typeof blogPostsArray };
     data.blogPosts = blogPostsArray;
+    return <PageComponent {...result} />;
+};
+
+export default function Page() {
     return (
-        <PageComponent  {...result}  />
-    )
+        <Suspense fallback={<div>Loading...</div>}>
+            <PageContent />
+        </Suspense>
+    );
 }
